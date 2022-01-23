155f87522c799fa706da82b389bb16ea553e547a
Jonatha Silveira <jonatha.psilveira@gmail.com>
2022-01-23 00:36:27 -0300

    Exercício_02

---
+++ Exercicio_02/src/dio/bootcamp/java/Main.java
@@ -0,0 +1,11 @@
+package dio.bootcamp.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\nExercício do Quadrilátero:");
+        Quadrilatero.area(5);
+        Quadrilatero.area(3d, 5d);
+        Quadrilatero.area(7, 5, 6);
+        Quadrilatero.area(7f, 8f);
+    }
+}
